75595ceb1fa2c641288acf9b37d180b2a46c36d7
HrachBarsa <154804578+HrachBarsa@users.noreply.github.com>
2024-03-13 17:36:03 +0400

    Update initDbSchema.sql

--- GalacticArchive.IndexingEngine/initDbSchema.sql
+++ GalacticArchive.IndexingEngine/initDbSchema.sql
@@ -1,1 +1,17 @@
-﻿
+CREATE TABLE Directories
+(
+Path varchar(256) primary key not null,
+Name varchar(256)  not null,
+DirectoryPath varchar(256) not null,
+LastModified datetime not null, 
+);
+CREATE TABLE Files
+(
+Path varchar(256) primary key not null,
+Name varchar(256)  not null,
+DirectoryPath varchar(256) not null,
+Size bigint,
+LastModified datetime not null, 
+);
+CREATE NONCLUSTERED INDEX IX_Directories_Name ON Directories (Name);
+CREATE NONCLUSTERED INDEX IX_Files_Name ON Files (Name);
